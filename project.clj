0b0d883dea9ab0981916dc19ebe109ab56a4816d
Craig McDaniel <cpmcdaniel@gmail.com>
2020-10-19 21:58:56 -0400

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bukkit-for-clojure/lein-template "0.1.1"
+(defproject bukkit-for-clojure/lein-template "0.1.2-SNAPSHOT"
   :description "A Leiningen template for creating Bukkit plugins in Clojure"
   :url "https://github.com/cpmcdaniel/bukkit-for-clojure-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
